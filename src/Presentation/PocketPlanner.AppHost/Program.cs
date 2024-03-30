d9f0d1937f185bb019a9d939dabb550da0079302
Shak-o <shalva.turashvili.1@btu.edu.ge>
2024-03-30 18:03:49 +0400

    add web client

--- src/Presentation/PocketPlanner.AppHost/Program.cs
+++ src/Presentation/PocketPlanner.AppHost/Program.cs
@@ -3,8 +3,13 @@ var builder = DistributedApplication.CreateBuilder(args);
 var sqlServer = builder.AddConnectionString("PocketPlannerDb");
 
 var pocketPlanner = builder.AddProject<Projects.PocketPlanner>("pocketplanner")
+    //.WithReplicas(3)
     .WithReference(sqlServer);
-var migrations = builder.AddProject<Projects.PocketPlanner_MigrationService>("pocketplannermigrations")
+
+builder.AddProject<Projects.PocketPlannerWeb>("pocketplannerweb")
+    .WithReference(pocketPlanner);
+
+builder.AddProject<Projects.PocketPlanner_MigrationService>("pocketplannermigrations")
     .WithReference(sqlServer);
 
 builder.Build().Run();
